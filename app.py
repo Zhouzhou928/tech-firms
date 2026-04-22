23b0ca2194af80a1e85152c06f6bfb8d7655a448
Zhouzhou928 <3294841089@qq.com>
2026-04-22 19:02:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="Advanced Financial Dashboard", layout="wide")
 
